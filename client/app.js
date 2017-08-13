3ea5c525896486ba603e79a1f950d70800ec29fd
Nequil Hill <nequil17@hotmail.com>
2017-08-13 15:46:25 -0500

    ...

--- client/app.js
+++ client/app.js
@@ -12,37 +12,31 @@ mainApp.config(function($routeProvider){
     })
 });
 
-mainApp.controller("CharacterController",['$scope', '$http','$location',function($scope,$http,$location){
-    console.log('i have been got')
-        console.log('git clicked')
+mainApp.controller("CharacterController",['$scope', '$http','$location',function($scope,$http,$location) {
         $http.get('http://localhost:3000/api/')
-            .then(function(response){
-                console.log(response.data)
+            .then(function(response) {
                 $scope.name=response.data
     });
-            $scope.getName=function(id){
-            console.log(id)
+            $scope.getName=function(id) {
             $location.path("/api/" + id)
         };
-            $scope.getClass=function(Class){
-            console.log(Class)
+            $scope.getClass=function(Class) {
             $location.path("/api/class/" + Class)
         };
-}])
-mainApp.controller("SingleController",['$scope', '$routeParams', '$http', function($scope, $routeParams, $http){
-     var id=$routeParams.id;
-    $http.get('http://localhost:3000/api/'+id)
-        .then(function(response){
-        console.log(response.data)
-        $scope.single=response.data
-        
+}]);
+
+mainApp.controller("ClassController",['$scope', '$routeParams', '$http', function($scope, $routeParams, $http) {
+    var role=$routeParams.Class;
+    $http.get('http://localhost:3000/api/role/' + Class)
+    .then(function(response) {
+        $scope.class=response.data
     });
 }]);
-mainApp.controller("ClassController",['$scope', '$routeParams', '$http', function($scope, $routeParams, $http){
-     var role=$routeParams.Role;
-    $http.get('http://localhost:3000/api/role/'+ Class)
-        .then(function(response){
-        console.log(response)
-        $scope.role=response.data
+
+mainApp.controller("SingleController",['$scope', '$routeParams', '$http', function($scope, $routeParams, $http) {
+    var id = $routeParams.id;
+    $http.get('http://localhost:3000/api/' + id)
+    .then(function(response) {
+        $scope.single = response.data
     });
 }]);
--- index.js
+++ index.js
@@ -10,77 +10,62 @@ app.use(express.static(clientPath));
 
 app.route('/api')
     .get(function(req,res){
-        console.log('get req')
-        res.sendFile(JSONPath);
+        fs.readFile(JSONPath, "utf-8", function(err, file){
+            return res.send(JSON.parse(file));
+        });
+        return res.sendFile(JSONPath);
     })
-app.route('/api/:id')
-    .get(function(req,res){
-        console.log(req)
-        console.log('get id')
-        fs.readFile(JSONPath, 'utf-8',function(err,file){
 
-            var arr = JSON.parse(file);
-            var id = req.params.id;
-            var result;
-
-            arr.forEach(function(element) {
-                if(element.id === id){
-                    result = element
-                }
-            });
-        if(result){
-            res.send(result)
-        }
-        })
-    }) 
-// app.route('/api/role/:Role')
-//     .get(function(req,res){
-//         console.log(req)
-//         console.log('get role')
-//         fs.readFile(JSONPath, 'utf-8',function(err,file){
-
-//             var arr = JSON.parse(file);
-//             var Role = req.query.Role;
-//             var result;
-
-//             arr.forEach(function(element) {
-//                 if(element.Role === Role){
-//                     result = element
-//                 }
-//             });
-//         if(result){
-//             res.send(result)
-//         }
-//         })
-//     }) 
-app.route('/api/role/:class')
-    .get(function(req, res){
-        fs.readFile(JSONPath, 'utf-8', function(err, fileContents) {
-            if (err) {
-                res.statusStatus(500);
-            } else {
-                var champions = JSON.parse(fileContents);
-                console.log(champions)
-                var Role = req.params.Class;
-                console.log(req.params.Class)
-                var response = names.filter(function(name) {
-                    if(champion.Class){
-                        if (name.Class.toLowerCase().trim() === Class.toLowerCase().trim()) {
+    
+app.route('/api/role/:Class')
+.get(function(req, res) {
+    fs.readFile(JSONPath, 'utf-8', function(err, fileContents) {
+        if (err) {
+            return res.statusStatus(500);
+        } else {
+            var name = JSON.parse(fileContents);
+            var Class = req.params.Class;
+            var response = name.filter(function(name) {
+                if(name.Class){
+                    if (name.Class.toLowerCase().trim() === Class.toLowerCase().trim()) {
                         return name;
                     }
-                    }
-
-                });
-                if (response) {
-                    res.send(response);
-                } else {
-                    res.sendStatus(404);
-                }
+                }     
+            });
+            if (response) {
+                return res.send(response);
+            } else {
+                return res.sendStatus(404);
+            }
+        }
+    });
+});
+    
+app.route('/api/:id')
+.get(function(req, res) { 
+    fs.readFile(JSONPath, 'utf-8', function(err, file) {
+        if (err) {
+        res.status(500).send('could not read file');
+        } else {
+    
+        var arr = JSON.parse(file);
+        var id = req.params.id;
+        var result;
+    
+        arr.forEach(function(character) {
+            if (character.id === id) {
+                result = character;
             }
         });
+        if (result === undefined) {
+                return res.sendStatus(404, 'Character not found');
+        } else {
+               return res.send(JSON.stringify(result));
+            }
+        }
     })
-
-
-
-app.listen(3000)
-
+})
+    
+    
+    app.listen(3000)
+    
